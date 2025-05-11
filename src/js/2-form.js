90864c13e9370f1da7229a9ec637c0d62f79805e
BaranivskyiOI <gakiygg@gmail.com>
2025-05-11 18:04:07 +0300

    do task

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const formData = {
+    email: "",
+    message: "",
+};
+
+const form = document.querySelector(".contact-form");
+const dataKey = "FORM_DATA" 
+
+try {
+
+    let {email: localEmail, message: localMessage} = JSON.parse(localStorage.getItem(dataKey));
+    
+    form.elements.email.value = localEmail;
+    form.elements.message.value = localMessage;
+    
+} catch (error) {
+    console.log(error);
+}
+
+
+form.addEventListener("input", (e) => {
+    
+    formData.email = form.elements.email.value;
+    formData.message = form.elements.message.value;
+    
+    
+    const stringifyFormData = JSON.stringify(formData)
+    
+    localStorage.setItem(dataKey, stringifyFormData);    
+    
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    localStorage.removeItem(dataKey);
+
+    e.target.reset();
+});
